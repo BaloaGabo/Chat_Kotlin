aadac7eca215be19574f53abf3f5843d78e2a358
gabri <gabrielbaloa.gb@gmail.com>
2025-01-16 00:55:55 +0100

    Parametros para el inicio de sesion con google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.loginGoogle)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
