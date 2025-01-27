595c7a9c3a3f418acc4c49c7ce34a7d2e56e91ac
gabri <gabrielbaloa.gb@gmail.com>
2025-01-27 22:58:08 +0100

    implementamos la funcionalidad de buscar usuarios en nuestro FragmentUsuarios.kt

--- app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentUsuarios.kt
+++ app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentUsuarios.kt
@@ -2,6 +2,8 @@ package com.example.chat_kotlin.Fragmentos
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -40,6 +42,20 @@ class FragmentUsuarios : Fragment() {
 
         usuarioLista = ArrayList()
 
+        binding.etBuscarUsuario.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(usuario: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                buscarUsuario(usuario.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
         listaUsuarios()
 
         return binding.root
@@ -52,10 +68,13 @@ class FragmentUsuarios : Fragment() {
         reference.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 (usuarioLista as ArrayList<Usuario>).clear()
-                for (sn in snapshot.children){
-                    val usuario : Usuario? = sn.getValue(Usuario::class.java)
-                    if(!(usuario!!.uid).equals(firebaseUser)){
-                        (usuarioLista as ArrayList<Usuario>).add(usuario)
+                if (binding.etBuscarUsuario.text.toString().isEmpty()){
+
+                    for (sn in snapshot.children){
+                        val usuario : Usuario? = sn.getValue(Usuario::class.java)
+                        if(!(usuario!!.uid).equals(firebaseUser)){
+                            (usuarioLista as ArrayList<Usuario>).add(usuario)
+                        }
                     }
                 }
                 usuarioAdaptador = AdaptadorUsuario(mContext, usuarioLista!!)
@@ -67,5 +86,29 @@ class FragmentUsuarios : Fragment() {
             }
         })
 
+    }
+
+    private fun buscarUsuario (usuario: String){
+        val firebaseUser = FirebaseAuth.getInstance().currentUser!!.uid
+        val reference = FirebaseDatabase.getInstance().reference.child("Usuarios").orderByChild("nombres")
+            .startAt(usuario).endAt(usuario+"\uf8ff")
+        reference.addValueEventListener(object  : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (usuarioLista as ArrayList<Usuario>).clear()
+                for (ss in snapshot.children){
+                    val usuario : Usuario ?= ss.getValue(Usuario::class.java)
+                    if(!(usuario!!.uid).equals(firebaseUser)){
+                        (usuarioLista as ArrayList<Usuario>).add(usuario)
+
+                    }
+                }
+                usuarioAdaptador = AdaptadorUsuario(mContext, usuarioLista!!)
+                binding.RVUsuarios.adapter = usuarioAdaptador
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
     }
 }
