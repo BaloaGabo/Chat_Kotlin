34fd27c88f59b0bc6bc9882e2617353293b3d04a
gabri <gabrielbaloa.gb@gmail.com>
2025-01-27 22:25:59 +0100

    Generamos el codigo para darle funcionalidad al FragmentUsuarios y validamos que solo se enlistaran los usuarios diferentes al usuario actual.

--- app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentUsuarios.kt
+++ app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentUsuarios.kt
@@ -1,20 +1,71 @@
 package com.example.chat_kotlin.Fragmentos
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.chat_kotlin.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chat_kotlin.AdaptadorUsuario
+import com.example.chat_kotlin.Usuario
+import com.example.chat_kotlin.databinding.FragmentUsuariosBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class FragmentUsuarios : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuarios, container, false)
+    private lateinit var binding : FragmentUsuariosBinding
+
+
+    private lateinit var mContext : Context
+    private var usuarioAdaptador : AdaptadorUsuario?=null
+    private var usuarioLista : List<Usuario>?=null
+
+    override fun onAttach(context: Context) {
+        mContext = context
+        super.onAttach(context)
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentUsuariosBinding.inflate(layoutInflater, container, false)
+
+        binding.RVUsuarios.setHasFixedSize(true)
+        binding.RVUsuarios.layoutManager = LinearLayoutManager(mContext)
+
+        usuarioLista = ArrayList()
+
+        listaUsuarios()
+
+        return binding.root
+    }
+
+    private fun listaUsuarios() {
+
+        val firebaseUser = FirebaseAuth.getInstance().currentUser!!.uid
+        val reference = FirebaseDatabase.getInstance().reference.child("Usuarios").orderByChild("nombres")
+        reference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                (usuarioLista as ArrayList<Usuario>).clear()
+                for (sn in snapshot.children){
+                    val usuario : Usuario? = sn.getValue(Usuario::class.java)
+                    if(!(usuario!!.uid).equals(firebaseUser)){
+                        (usuarioLista as ArrayList<Usuario>).add(usuario)
+                    }
+                }
+                usuarioAdaptador = AdaptadorUsuario(mContext, usuarioLista!!)
+                binding.RVUsuarios.adapter = usuarioAdaptador
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+
     }
 }
