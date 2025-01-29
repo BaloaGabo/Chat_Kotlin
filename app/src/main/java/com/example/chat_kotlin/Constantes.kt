1afb5b8d512caec4a84d3d4f7a74c36074d787e0
gabri <gabrielbaloa.gb@gmail.com>
2025-01-29 04:58:07 +0100

    Implementamos la funcion cargarInfo() que nos permite visualizar la imagen de perfil y el nombre en el chat

--- app/src/main/java/com/example/chat_kotlin/Constantes.kt
+++ app/src/main/java/com/example/chat_kotlin/Constantes.kt
@@ -2,6 +2,7 @@ package com.example.chat_kotlin
 
 import android.provider.CalendarContract.Calendars
 import android.text.format.DateFormat
+import java.util.Arrays
 import java.util.Calendar
 import java.util.Locale
 
@@ -17,6 +18,14 @@ object Constantes {
         calendar.timeInMillis = tiempo
 
         return DateFormat.format("dd/MM/yyyy",calendar).toString()
+    }
+
+    fun rutaChat(receptorUid : String, emisorUid : String) : String{
+        val arrayUid = arrayOf(receptorUid, emisorUid)
+        Arrays.sort(arrayUid)
+
+
+        return "${arrayUid[0]}_${arrayUid[1]}"
     }
 
 }
